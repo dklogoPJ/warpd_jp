d1ad6911046a059aa3a28e1bd42b147342f4f69f
David Klogo <klogodavid@gmail.com>
2023-07-30 19:44:19 +0000

    credit sales

--- app/app/webroot/js/scripts/omc_customer/credit_sales.js
+++ app/app/webroot/js/scripts/omc_customer/credit_sales.js
@@ -37,10 +37,10 @@ var Order = {
                 {display:'Invoice Date.', name:'invoice_date', width:100, sortable:false, align:'left', hide:false, editable:{form:'text', validate:'empty', placeholder:'dd-mm-yyyy',bclass:'datepicker', maxlength:'10', defval:jLib.getTodaysDate('mysql_flip')}},
                 {display:'Product Type', name:'product_type_id', width:150, sortable:true, align:'left', hide:false, editable:{form:'select', validate:'', defval:'', options:products}},
                 {display:'Sales Quantity (ltr)', name:'sales_qty', width:140, sortable:false, align:'left', hide:false, editable:{form:'text', validate:'', defval:'', on_key_up:'{"action":"multiply", "sources":["sales_qty","price"], "targets":["sales_amount"]}'}},
-                {display:'Price', name:'price', width:90, sortable:true, align:'left', hide:false, editable:{form:'text', validate:'', defval:''}},
+                {display:'Price', name:'price', width:70, sortable:true, align:'left', hide:false, editable:{form:'text', validate:'', defval:''}},
                 {display:'Delivery Method', name:'delivery_method', width:150, sortable:true, align:'left', hide:false, editable:{form:'select', validate:'', defval:'', options:delivery_method}},
                 {display:'Sales Amount (GHs.)', name:'sales_amount', width:130, sortable:true, align:'left',format_number:true, hide:false, editable:{form:'text',readonly:'readonly', validate:'', defval:''}},
-                {display:'Staff Name', name:'staff_name', width:100, sortable:false, align:'left', hide:false, editable:{form:'text', validate:'', defval:''}},
+                {display:'Staff Name', name:'staff_name', width:120, sortable:false, align:'left', hide:false, editable:{form:'text', validate:'', defval:''}},
                 {display:'Comments', name:'comments', width:170, sortable:true, align:'left', hide:false, editable:{form:'text', validate:'', defval:''}}
             ],
             formFields:btn_actions,
