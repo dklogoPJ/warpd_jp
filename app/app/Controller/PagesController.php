3183328f354962174e017bd4b46ce8b875970f43
Gideon Amissah <gideon.amissah@topgolf.com>
2023-09-23 09:56:49 +0000

    JP-Cake_2_10 Upgrading Base cakephp core files to ver 2.10.24

--- app/app/Controller/PagesController.php
+++ app/app/Controller/PagesController.php
@@ -4,19 +4,18 @@
  *
  * This file will render views from views/pages/
  *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
  * @package       app.Controller
  * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 
 App::uses('AppController', 'Controller');
@@ -27,54 +26,55 @@ App::uses('AppController', 'Controller');
  * Override this controller by placing a copy in controllers directory of an application
  *
  * @package       app.Controller
- * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
+ * @link https://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
-class PagesController extends AppController
-{
+class PagesController extends AppController {
 
-    /**
-     * Controller name
-     *
-     * @var string
-     */
-    public $name = 'Pages';
+/**
+ * This controller does not use a model
+ *
+ * @var array
+ */
+	public $uses = array();
 
-    /**
-     * This controller does not use a model
-     *
-     * @var array
-     */
-    public $uses = array();
+/**
+ * Displays a view
+ *
+ * @return CakeResponse|null
+ * @throws ForbiddenException When a directory traversal attempt.
+ * @throws NotFoundException When the view file could not be found
+ *   or MissingViewException in debug mode.
+ */
+	public function display() {
+		$path = func_get_args();
 
-    /**
-     * Displays a view
-     *
-     * @param mixed What page to display
-     * @return void
-     */
-    public function display()
-    {
-        //$this->redirect('Dashboard');
-        $this->redirect(array('controller' => 'Dashboard', 'action' => 'index'));
+		$count = count($path);
+		if (!$count) {
+			return $this->redirect('/');
+		}
+		if (in_array('..', $path, true) || in_array('.', $path, true)) {
+			throw new ForbiddenException();
+		}
+		$page = $subpage = $title_for_layout = null;
 
-        $path = func_get_args();
+		if (!empty($path[0])) {
+			$page = $path[0];
+		}
+		if (!empty($path[1])) {
+			$subpage = $path[1];
+		}
+		if (!empty($path[$count - 1])) {
+			$title_for_layout = Inflector::humanize($path[$count - 1]);
+		}
+		$this->set(compact('page', 'subpage', 'title_for_layout'));
 
-        $count = count($path);
-        if (!$count) {
-            $this->redirect('/');
-        }
-        $page = $subpage = $title_for_layout = null;
-
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        if (!empty($path[$count - 1])) {
-            $title_for_layout = Inflector::humanize($path[$count - 1]);
-        }
-        $this->set(compact('page', 'subpage', 'title_for_layout'));
-        $this->render(implode('/', $path));
-    }
+		try {
+			$this->render(implode('/', $path));
+		} catch (MissingViewException $e) {
+			if (Configure::read('debug')) {
+				throw $e;
+			}
+			throw new NotFoundException();
+		}
+	}
 }
